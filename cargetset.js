328f8570abe3941bedbd85784ef147ad6593930c
Biba2023 <bibautepova@gmail.com>
2023-06-06 16:11:36 +0600

    add mobile-first home work

--- cargetset.js
+++ cargetset.js
@@ -1,37 +1,103 @@
-class Car {
-  constructor (nameCar, maxSpeed, currentSpeed){
-    this._name = nameCar;
-    this._maxSpeed = maxSpeed;
-    this._currentSpeed = currentSpeed;
+// class Car {
+//   constructor (nameCar, maxSpeed, currentSpeed){
+//     this.name = nameCar;
+//     this.maxSpeed = maxSpeed;
+//     this.currentSpeed = currentSpeed;
+//   }
+//   get nameCar(){
+//     return this._nameCar;
+//   }
+//   set nameCar (nameCar){
+//     this._nameCar = nameCar;
+//   }
+//   get maxSpeed (){
+//     return this._maxSpeed;
+//   }
+//   set maxSpeed (maxSpeed){
+//     if (maxSpeed > 0){
+//       this._maxSpeed = maxSpeed;
+//     }
+//   }
+//   get currentSpeed(){
+//     return this._currentSpeed;
+//   }
+//   fastSpeed (speed){
+//     if (speed > 0){
+//       return(`fastSpeed ${speed}`);
+//     } 
+//   }
+//   brakeCar() {
+//     this._currentSpeed = 0;
+//     return('Stop');
+//   }
+// }
+
+
+
+// class Circle {
+//   constructor(radius) {
+//     this._radius = radius;
+//   }
+//   get radius() {
+//     return this._radius;
+//   }
+//   set radius(radius) {
+//     if (radius >= 0) {
+//       this._radius = radius;
+//     }
+//   }
+//   getArea() {
+//     const area = Math.PI * Math.pow(this._radius, 2);
+//     return area.toFixed(2); 
+//   }
+// }
+// const circle1 = new Circle(5);
+// function getFigureArea (figure){
+//   if(figure instanceof Figure){
+//     return figure.getArea();
+//   }
+//   throw new TypeError ('You must give proper figure')
+// }
+
+
+class Animal {
+  constructor(name){
+    this.name = name;
+    this.isHungry = false;
   }
-  get nameCar(){
-    return this._nameCar;
+  move (){
+    console.log(`Animal ${this.name}is Moving`);
   }
-  set nameCar (nameCar){
-    this._nameCar = nameCar;
+  eat() {
+    console.log(`Animal ${this.name}is eating food`);
+    this.isHungry = false;
   }
-  get maxSpeed (){
-    return this._maxSpeed;
-  }
-  set maxSpeed (maxSpeed){
-    if (maxSpeed > 0){
-      this._maxSpeed = maxSpeed;
-    }
-  }
-  get currentSpeed(){
-    return this._currentSpeed;
-  }
-  fastSpeed (speed){
-    if (speed > 0){
-      return(`fastSpeed ${speed}`);
-    } 
-  }
-  brakeCar() {
-    this._currentSpeed = 0;
-    return('Stop');
+  speak() {
+    console.log(`Animal ${this.name}making noices`);
   }
 }
-
+class Cat extends Animal {
+  constructor (name){
+    super(name);
+  }
+  move(){
+    console.log(`Cat ${this.name}is runnig. Poor vase`);
+  }
+  eat (){
+    console.log(`Cat ${this.name}is eating mice`);
+  }
+  speak(){
+    console.log(`Cat ${this.name}is meowing`);
+  }
+}
+class Dog extends Animal {
+  constructor(name){
+    super(name);
+  }
+  move(){
+    console.log(`...`);
+  }
+}
 
 
 
